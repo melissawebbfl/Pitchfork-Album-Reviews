25048cf3b878a9802a148b48cc33e691518165be
Melissa <25356132+melissawebbfl@users.noreply.github.com>
2022-02-06 00:36:50 -0500

    changed tab size

--- pitchfork_data_exploration.sql
+++ pitchfork_data_exploration.sql
@@ -3,7 +3,7 @@
 	Melissa Webb
     
 	Description: This database contains information related to over 18,000 music album reviews posted on a website called Pitchfork between the years 1999 and 2017.
-    	The following tables are contained in the database: reviews, artists, genres, labels
+    The following tables are contained in the database: reviews, artists, genres, labels
 	
 	Skills used: CTE's, joins, aggregate functions, subqueries, partition by, group by
 	
